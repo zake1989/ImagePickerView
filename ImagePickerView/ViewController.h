ee42ab6fb2f6d826a528f46897bc04bdea9f43c7
Yukai Zeng <zengyukai1@gmail.com>
2015-07-17 13:53:03 -0700

    Start Project Image Picker

---
+++ ImagePickerView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImagePickerView
+//
+//  Created by Stephen on 7/17/15.
+//  Copyright (c) 2015 Zake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ImagePickerViewTests/ImagePickerViewTests.m
@@ -0,0 +1,40 @@
+//
+//  ImagePickerViewTests.m
+//  ImagePickerViewTests
+//
+//  Created by Stephen on 7/17/15.
+//  Copyright (c) 2015 Zake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ImagePickerViewTests : XCTestCase
+
+@end
+
+@implementation ImagePickerViewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
